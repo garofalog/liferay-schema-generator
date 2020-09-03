6901f2bd75087acc856d3510ed808459a0832e41
Giulio Garofalo <giulio.garofalo@liferay.com>
2020-09-03 13:12:48 +0200

    Updates

---
+++ precache-manifest.8e13e41ae7a22a3d357c1a80be66d7ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c17efaf6dcc8a5357f9c8fb2c50e44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0e9dff54f554902cec",
+    "url": "/static/css/main.7278b481.chunk.css"
+  },
+  {
+    "revision": "2a3480788e03de697cfd",
+    "url": "/static/js/2.e540bd74.chunk.js"
+  },
+  {
+    "revision": "db0e9dff54f554902cec",
+    "url": "/static/js/main.1eb98448.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/garofalog/liferay-db-schema-generator/diagram-db-react/precache-manifest.3dde28ce9d60a35cb74243085e39c343.js"
+  "/precache-manifest.8e13e41ae7a22a3d357c1a80be66d7ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/garofalog/liferay-db-schema-generator/diagram-db-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
